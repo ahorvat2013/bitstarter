f48616ee113f066eb64ef7051d2fb70bcf310cb7
ahorvat2013 <aurelia.horvat@gmail.com>
2013-07-10 21:34:43 +0000

    read from index.html

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var buffer = fs.readFileSync("index.html");
+  response.send(buffer.toString('utf-8') /*'Hello World 2!'*/);
 });
 
 var port = process.env.PORT || 5000;
